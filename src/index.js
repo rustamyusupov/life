2861cf09fa59845a2a3079301b87049d8540e356
Rustam Yusupov <i@rstm.me>
2025-05-02 11:59:02 +0200

    Refactor version rendering into separate function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const init = async () => {
+const renderVersion = () => {
   const version = document.getElementById("version");
-
   version.textContent = __APP_VERSION__;
+};
+
+const init = () => {
+  renderVersion();
 };
 
 document.addEventListener("DOMContentLoaded", init);
